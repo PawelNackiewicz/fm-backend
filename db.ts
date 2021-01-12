45cd699dde1f3cbf8a3c3c646d7bbf20daed5fcd
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-01-12 21:15:49 +0100

    :card_file_box: add prisma

---
+++ db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+export const initDb = async () => {
+  const result = await client.$queryRaw<readonly object[]>`SELECT 1=1 AS "database ready";`;
+  return result[0];
+};
+
+/**
+ * @description Use `request.server.app.db` instead
+ */
+export const prisma = client; // as never;
--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
 import { getServerWithPlugins } from './server';
+import type { PrismaClient } from '@prisma/client';
+import * as Dotenv from 'dotenv';
+import {initDb, prisma} from "./db";
+
+Dotenv.config();
+
+declare module '@hapi/hapi' {
+    export interface ServerApplicationState {
+        readonly db: PrismaClient;
+    }
+}
 
 const start = async () => {
-    //TODO config prisma and DB
+    console.log(await initDb());
     const server = await getServerWithPlugins();
+
+    // @ts-expect-error
+    server.app.db = prisma;
+
     await server.start();
     console.info('Server running at:', server.info.uri);
 };
